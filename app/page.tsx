7435830c1255224ae37d05860d8a3828418341eb
Mr.Sauji💸 <maharjangagan2057@gmail.com>
2026-01-09 18:26:39 +0545

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="display flex h-screen  w-screen items-center justify-between ">
-    <div className="w-[50%] h-screen bg-amber-200">
+    <div className="w-[50%] h-screen bg-red-200">
       
     </div>
     <div className="w-[50%] h-screen bg-pink-200">
